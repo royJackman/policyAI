2be32e6afaa9e35214133d98e6a6717fde981d30
Roy Jackman <jackman.roy@gmail.com>
2018-08-18 00:39:46 +0300

    Class init
    Basic class for elections. Will be mainly functional, will spawn an election object to perform the election then will be deleted

--- election.py
+++ election.py
@@ -4,3 +4,10 @@ TODO:
 - Differnet electoral systems
 - Misscounts
 '''
+
+class election:
+    def __init__(self,type,exclusivity,time,mailIn):
+        self.type = type
+        self.exclusivity = exclusivity
+        self.time = time
+        self.mailIn = mailIn
