137e8f3e5129f5e275bf050534f7fcd537af8b28
Roy Jackman <jackman.roy@gmail.com>
2018-08-18 00:31:01 +0300

    Simple __init__
    Added a very basic __init__, taxes will be objects, saved, and parsed later

--- taxes.py
+++ taxes.py
@@ -4,3 +4,9 @@ TODO:
 - Tax dispersion calculation
 - Loophole integration
 '''
+
+class taxes:
+    def __init__(self,effectedRanges,initDate,returnOnInvestment):
+        self.effectedRanges = effectedRanges
+        self.initDate = initDate
+        self.returnOnInvestment = returnOnInvestment
