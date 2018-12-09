06327b6807eacf4b473dc1dcdc1aad52d436cf3e
Roy Jackman <jackman.roy@gmail.com>
2018-12-09 13:23:29 -0500

    main refresh 3.6.1 fix

--- main.py
+++ main.py
@@ -3,38 +3,37 @@ import json
 import election
 import generator
 
-import tensorflow as tf
+# import tensorflow as tf
 
-# import keras.backend as K
+import keras.backend as K
 
-# from keras.models import Sequential
-# from keras.layers import Dense, Activation
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 x = open("information/people.json", "r")
 pop = json.load(x)
 x.close()
 
-c = 10000
+c = 100
 
 x = generator.policyGen(count=c)
 y = generator.publicOpinion(pop, x)
 x = generator.policyGen(count=c, data=True, translator=True, previous=x)
 
-# model = Sequential()
-# model.add(Dense(8, input_shape=(13,)))
-# model.add(Activation("relu"))
-# model.add(Dense(4, input_shape=(8,)))
-# model.add(Activation("relu"))
-# model.add(Dense(1, input_shape=(4,)))
-# model.add(Activation("softmax"))
+model = Sequential()
+model.add(Dense(8, input_shape=(13,)))
+model.add(Activation("relu"))
+model.add(Dense(4, input_shape=(8,)))
+model.add(Activation("relu"))
+model.add(Dense(1, input_shape=(4,)))
+model.add(Activation("relu"))
 
-# model.compile(optimizer="rmsprop", loss="mae", metrics=['mae','accuracy'])
+model.compile(optimizer="rmsprop", loss="mse", metrics=['mae'])
 
-# scale = int(0.8*c)
+scale = int(0.8*c)
 
-# model.fit(x[:scale], y[:scale], epochs=20, batch_size=400, verbose=2)
+model.fit(x[:scale], y[:scale], epochs=5, batch_size=4, verbose=1)
 
-# print(model.evaluate(x[scale:], y[scale:]))
-
+print(model.evaluate(x[scale:], y[scale:]))
